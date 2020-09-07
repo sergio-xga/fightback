d573ebc287772896adb942ae9f43deb77dbd7dfc
sergio-xga <sergio.xga@gmail.com>
2020-09-07 17:58:39 -0600

    Redirect non-existent routes to root, except for activestorage

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :projects do
     resources :donations
   end
+  get '*all', to: 'web_app#landing', constraints: lambda { |req|
+    req.path.exclude? 'rails/active_storage'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
