2fcd20c30b3769acc9e0440f0f4474fb255bf8f9
sergio-xga <sergio.xga@gmail.com>
2020-09-12 21:25:23 -0600

    Fix problem with BootstrapTables when going to previous page using the browser

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,6 +19,9 @@ import "../stylesheets/application"
 $(document).on("turbolinks:load", function() {
   $('[data-toggle="table"]').bootstrapTable();
 });
+$(document).on("turbolinks:before-cache", function() {
+  $('[data-toggle="table"]').bootstrapTable('destroy');
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
